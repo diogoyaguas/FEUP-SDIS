73377de0f3621d84ddd2fb351b696fc2a936fc88
Alexandra <up201604741@fe.up.pt>
2019-03-15 09:54:04 +0000

    Started working on Project

---
+++ P1 - Serverless Distributed Backup Service/src/client.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+import java.lang.Thread;
+import java.util.TimerTask;
+import java.util.Timer;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class client {
+
+    public static void main(String args[]) throws IOException {
+
+    }
+}
